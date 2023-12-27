0e86f6181f34870f51ad5b16fca21a0be771098e
eugenehan94 <eugene.han94@gmail.com>
2023-12-27 16:43:47 -0500

    adding question 4 to cracking the coding interview

--- cracking-the-coding-interview/chapter1-arraysAndStrings/question4.js
+++ cracking-the-coding-interview/chapter1-arraysAndStrings/question4.js
@@ -9,6 +9,63 @@ input: Tact Coa
 output: True (permuations: "taco cat", "atco cta", etc)
 */
 
-const myAttempt = (str) => {
-    
-}
+const isPalindrome = (str) => {
+  const charCount = {};
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    // Run if statement for ASCII outside lowercase letters a to z
+    if (
+      (str[i].charCodeAt() & 255) < ("a".charCodeAt() & 255) ||
+      (str[i].charCodeAt() & 255) > ("z".charCodeAt() & 255)
+    ) {
+      continue;
+    }
+    // store in charCount the letter counts: charCount = { t: 2, e: 1, s:1}
+    charCount[str[i]] = (charCount[str[i]] || 0) + 1;
+  }
+
+  // Generates an array using the values of the Object (not the keys)
+  // Example -> counts = [2, 1, 1]
+  const counts = Object.values(charCount);
+  let oddCounts = 0;
+  for (let i = 0; i < counts.length; i++) {
+    if (counts[i] % 2 != 0) {
+      oddCounts++;
+    }
+
+    if (oddCounts > 1) {
+      return false;
+    }
+  }
+  return true;
+};
+//O(2N) -> O(N) Using 1 loop
+const isPalindromeShort = (str) => {
+  const charCount = {};
+  let oddCounts = 0;
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (
+      (str[i].charCodeAt() & 255) < ("a".charCodeAt() & 255) ||
+      (str[i].charCodeAt() & 255) > ("z".charCodeAt() & 255)
+    )
+      continue;
+
+    charCount[str[i]] = (charCount[str[i]] || 0) + 1;
+
+    if (charCount[str[i]] % 2 == 1) {
+      oddCounts++;
+    } else {
+      oddCounts--;
+    }
+  }
+  return oddCounts <= 1;
+};
+
+console.log(isPalindrome("test"));
+console.log(isPalindrome("tenet"));
+console.log(isPalindrome("taco caT"));
+console.log("------other function--------");
+console.log(isPalindromeShort("test"));
+console.log(isPalindromeShort("tenet"));
+console.log(isPalindromeShort("taco caT"));
