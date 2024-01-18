3014bf7b0aa43287566b14e4b0924b22c4431b67
eugenehan94 <eugene.han94@gmail.com>
2024-01-18 13:42:06 -0500

    working on question 8 chapter 2

--- cracking-the-coding-interview/chapter2/question8.js
+++ cracking-the-coding-interview/chapter2/question8.js
@@ -26,20 +26,20 @@ test.next.next.next = new Node("D");
 test.next.next.next.next = new Node("E");
 test.next.next.next.next.next = test.next.next;
 
-
-console.log("------Approach 1 (Website) ---------")
+console.log("------Approach 1 (Website) ---------");
 
 /*
 1. If a loop is found, initialize a slow pointer to head, let fast pointer be at its position. 
 2. Move both slow and fast pointers one node at a time. 
 3. The point at which they meet is the start of the loop.
+
+Time complexity: O(n), where n is length of array
+Auxiliary space: O(1)
 */
 
-
 function detectAndRemove(head) {
-
   // If list is empty or has only one node - no loop
-  if (head === null || head.next === null){
+  if (head === null || head.next === null) {
     return null;
   }
 
@@ -51,6 +51,82 @@ function detectAndRemove(head) {
   fast = fast.next.next;
 
   // Search for loop using slow and fast pointers
-  
+  while (fast !== null && fast.next !== null) {
+    if (slow === fast) {
+      break;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
 
+  // If loop doesn't exist
+  if (slow !== fast) {
+    return null;
+  }
+
+  // If loop exists. Start slow from head and fast
+  // from meeting point.
+  slow = head;
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
 }
+
+console.log(detectAndRemove(test));
+
+console.log("---------Approach 2 (Website)-----------");
+
+/*
+In this method, a temporary node is created. The next pointer 
+of each node that is traversed is made to point to this temporary 
+node. This way we are using the next pointer of a node as a flag to 
+indicate whether the node has been traversed or not. Every node is 
+checked to see if the next is pointing to a temporary node or not. 
+In the case of the first node of the loop, the second time we traverse 
+it this condition will be true, hence we return that node. 
+The code runs in O(n) time complexity and uses constant memory space.
+
+Steps to solve this problem:
+1.) Create a temporary node and a pointer temp.
+2.) While head is not null:
+*Check if head -> next is null then return null.
+*Check if head -> next is temp than break.
+*Create a next pointer to a node and point it to head -> next.
+*Point head -> next to temp.
+*Update head to next.
+
+Return head.
+
+*/
+
+function detectLoop(head) {
+  // Create a temporary node
+  temp = new Node();
+
+  while (head !== null) {
+    // This condition is for the case when there is no loop
+    if (head.next === null) {
+      return null;
+    }
+
+    // Check if next is already pointing to temp
+    if (head.next === temp) {
+      break;
+    }
+    // Store the pointer to the next node in order to get to it
+    // in the next step
+    next = head.next;
+
+    // Make next pointer to temp
+    head.next = temp;
+
+    // Get the next node in the list
+    head = next;
+  }
+  return head;
+}
+
+console.log(detectLoop(test));
