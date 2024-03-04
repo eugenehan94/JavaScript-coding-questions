087d290eff05eef019c16bfe77abb9458440197a
eugenehan94 <eugene.han94@gmail.com>
2024-03-04 13:11:50 -0500

    added adjacency list graph example

--- references/graph.js
+++ references/graph.js
@@ -1,10 +1,10 @@
 // Adjacency List as a Hashmap
-const graph = {
-  a: ["a", "b"],
-  b: ["c"],
-  c: ["d"],
-  d: ["b", "c"],
-};
+// const graph = {
+//   a: ["a", "b"],
+//   b: ["c"],
+//   c: ["d"],
+//   d: ["b", "c"],
+// };
 
 class Node {
   constructor(value) {
@@ -66,6 +66,29 @@ class Graph {
     }
     return [sourceNode, destinationNode];
   }
+  getNodes(value) {
+    return this.nodes.get(value);
+  }
+  /*
+We are using JavaScript generators, notice the * in 
+front of the function. This generator iterates one 
+value at a time. 
+*/
+  *bfs(first) {
+    const visited = new Map();
+    const visitList = new Queue();
+
+    visitList.add(first);
+
+    while (!visitList.isEmpty()) {
+      const node = visitList.remove();
+      if (node && !visited.has(node)) {
+        yield node;
+        visited.set(node);
+        node.getAdjacents().forEach((adj) => visitList.add(adj));
+      }
+    }
+  }
 }
 
 Graph.UNDIRECTED = Symbol("undirected graph");
@@ -74,4 +97,49 @@ Graph.DIRECTED = Symbol("directed graph");
 const test = new Graph(Graph.UNDIRECTED);
 const [first] = test.addEdge(1, 2);
 test.addEdge(1, 3);
+test.addEdge(1, 4);
+test.addEdge(5, 2);
+test.addEdge(6, 3);
+test.addEdge(7, 3);
+test.addEdge(8, 4);
+test.addEdge(9, 5);
+test.addEdge(10, 6);
 console.log(test);
+// console.log("test[1]: ", test.getNodes(5));
+
+/**
+ * Data structure where add and remove elements in a first-in, first-out (FIFO)
+ */
+class Queue {
+  constructor() {
+    this.items = new LinkedList();
+  }
+
+  enqueue(item) {
+    this.items.addLast(item);
+    return this;
+  }
+
+  dequeue() {
+    return this.items.removeFirst();
+  }
+
+  get size() {
+    return this.items.size;
+  }
+
+  isEmpty() {
+    return !this.items.size;
+  }
+}
+
+// const queue = new Queue();
+
+//queue.enqueue('a');
+//queue.enqueue('b');
+//queue.dequeue(); //↪️ a
+//queue.enqueue('c');
+//queue.dequeue(); //↪️ b
+//queue.dequeue(); //↪️ c
+// end::snippet[]
+// */
